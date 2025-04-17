510d4e016494cc3407f8b8e881023ae694e89656
zyo <zyond26@gmail.com>
2025-04-17 20:43:53 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyWebSite.Models;
+
+namespace MyWebSite.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Projects()
+    {
+        return View();
+    }
+
+    public IActionResult Skills()
+    {
+        return View();
+    }
+
+    public IActionResult Info()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Controllers/Page404Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebSite.Controllers;
+
+public class Page404Controller : Controller
+{
+    public IActionResult index()
+    {
+        return View();
+    }
+}
